b01a6c177ed98166ea30d521c7266b4a119ccb6e
Matt <matias_moll@yahoo.com.ar>
2020-09-29 01:13:31 -0300

    Added panel for work order numbers and type. Added examples after some research.

--- components/home/Home.js
+++ components/home/Home.js
@@ -7,6 +7,17 @@ export default function Home() {
   return (
     <View style={styles.container}>
       <Header title="Orden de Trabajo"/>
+      <View style={styles.topbarWorkOrder}>
+        <View style={styles.topbarWorkOrderNumbers}>
+          <Text style={styles.workOrderNumber}>Nro Orden: 154</Text>
+          <Text style={styles.workOrderQuantity}>1/15</Text>
+        </View>
+        <View style={styles.workOrderTypeContainer}>
+          <Text style={styles.workOrderType}>Reconexión Cable</Text>
+        </View>
+
+      </View>
+        
 
       <Navigation/>
     </View>
@@ -18,4 +29,29 @@ const styles = StyleSheet.create({
     flex:1,
     backgroundColor:'#ecf0f1'
   },
+  topbarWorkOrder:{
+    margin:8,
+    marginTop:15,
+    elevation: 2,
+    borderLeftColor:'#27AE60',
+    borderLeftWidth:3,
+  },
+  topbarWorkOrderNumbers: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding:10,
+    paddingLeft:20,
+    paddingRight:20,
+  },
+  workOrderNumber: {
+    fontSize:20,
+    fontWeight:'bold',
+  },
+  workOrderTypeContainer: {
+    paddingTop:10,
+    padding: 20
+  },
+  workOrderType: {
+    fontSize:24
+  }
 })
